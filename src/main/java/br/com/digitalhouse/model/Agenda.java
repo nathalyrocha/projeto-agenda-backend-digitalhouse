0c7b91998e04803fa5e7d5874a721c184d120adc
nathaly <nathalyfroliveira@gmail.com>
2020-09-30 19:53:41 -0300

    Código do projeto Agenda Backend

---
+++ src/main/java/br/com/digitalhouse/model/Agenda.java
@@ -0,0 +1,62 @@
+package br.com.digitalhouse.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "agenda")
+public class Agenda {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+	@Column(name = "telefone") //Sem o parêntese ele pega o nome da variável por default(Ex: Em String nome, o nome por default vai ser nome).
+	private String telefone;
+	
+	public Agenda() {}
+
+	public Agenda(Long id, String nome, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public String toString() {
+		return "Agenda [id=" + id + ", nome=" + nome + ", telefone=" + telefone + "]";
+	}
+	
+}
